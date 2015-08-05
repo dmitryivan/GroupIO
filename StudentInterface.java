b88379a36b14ef63a49e4d67165fad9cf5ec1fbe
dmitryivan <onemore43@gmail.com>
2015-08-05 17:51:02 +0300

    Create StudentInterface.java

---
+++ StudentInterface.java
@@ -0,0 +1,10 @@
+
+public interface StudentInterface {
+String info();
+void setContract(String contract);
+String getContract();
+int getAge();
+String getName();
+String getGender();
+void setSortCriteria(String criteria);
+}
